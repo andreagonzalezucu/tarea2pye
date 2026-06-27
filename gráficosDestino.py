1aedbcf1218dfc2f25ab7f5f1c43a42735454e74
andreagonzalezucu <andrea.gonzalezr@correo.ucu.edu.uy>
2026-06-27 18:24:48 +0000

    subpartes 1 a 3

---
+++ gráficosDestino.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("muestra2000.csv")
+
+# Frecuencias de la variable Destino
+frecuencia = df["Destino"].value_counts()
+
+# Gráfico de barras
+plt.figure(figsize=(10,6))
+frecuencia.plot(kind="bar")
+plt.title("Frecuencia de destinos")
+plt.xlabel("Destino")
+plt.ylabel("Frecuencia")
+plt.xticks(rotation=45)
+
+plt.tight_layout()
+plt.savefig("grafico_barras.png", dpi=300, bbox_inches="tight")
+plt.close()
+
+plt.figure(figsize=(8,8))
+frecuencia.plot(
+    kind="pie",
+    autopct="%1.1f%%",
+    startangle=90
+)
+
+plt.title("Distribución de destinos")
+plt.ylabel("")  # Oculta la etiqueta del eje Y
+plt.tight_layout()
+plt.savefig("grafico_circular.png", dpi=300, bbox_inches="tight")
+plt.close()
---
+++ tablasRegistroDestino.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv("muestra2000.csv")
+
+frecuencia_abs = df["Destino"].value_counts().sort_index()
+
+# Frecuencia relativa
+frecuencia_rel = df["Destino"].value_counts(normalize=True).sort_index()
+
+# Crear la tabla
+tabla_frecuencias = pd.DataFrame({
+    "Frecuencia absoluta": frecuencia_abs,
+    "Frecuencia relativa": frecuencia_rel
+})
+
+print(tabla_frecuencias)
